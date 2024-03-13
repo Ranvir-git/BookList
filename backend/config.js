d4ba427efd7408c2022eb8682f1b2e0cdd110b4e
Ranvir kumar <101629099+Ranvir-git@users.noreply.github.com>
2024-03-13 14:30:11 +0530

    this is first

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://ranvirkrspj2018:ranvir123@cluster0.oyaihg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
+const App = () => {
+  return (
+    <div className=' bg-yellow-200 w-full h-screen'>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
+    </div>
+    
+  );
+};
+
+export default App;
